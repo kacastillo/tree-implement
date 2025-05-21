aa2a72d1e098259e7563717ca283edf43ebfa937
Kelley Castillo <kelleycastillo@Kelleys-MacBook-Pro.local>
2025-05-21 11:52:48 -0700

    added

--- src/Traversal.java
+++ src/Traversal.java
@@ -1,5 +1,25 @@
 public class Traversal {
   public static void main(String[] args) {
+    TreeNode root = new TreeNode(10);
 
+    root.left = new TreeNode(9);
+    root.right = new TreeNode(15);
+
+    root.left.left = new TreeNode(5);
+    root.left.right = new TreeNode(2);
+
+    root.right = new TreeNode(15);
+    root.right.left = new TreeNode(-3);
+    root.right.right = new TreeNode(5);
+    root.right.right.right = new TreeNode(22);
+    preorder(root);
+    }
+
+    public static void preorder(TreeNode current) {
+      if (current == null) return;
+      System.out.println(current.data);
+      preorder(current.left);
+      preorder(current.right);
+    }
   }
-}
+  
